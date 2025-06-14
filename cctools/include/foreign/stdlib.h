1e2f3325aa8a22ba9c57fc4a7aef43b922f5389b
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-06-13 21:47:16 -0600

    Provide a realpath implementation for windows builds
    This is adapted from the patch written by LongDirtyAnimAlf
    https://github.com/LongDirtyAnimAlf/ld64-msys

--- cctools/include/foreign/stdlib.h
+++ cctools/include/foreign/stdlib.h
@@ -9,8 +9,68 @@
 #undef __STRICT_ANSI__
 #endif /* __CYGWIN__ */
 
+#ifdef _WIN32
+#define _GNU_SOURCE  // For vasprintf
+#endif
+
 #include_next <stdlib.h>
 
 #ifdef __CYGWIN__
 #pragma pop_macro("__STRICT_ANSI__")
 #endif /* __CYGWIN__ */
+
+#ifdef _WIN32
+#include <sys/stat.h>
+#include <errno.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+* Windows realpath implementation
+*
+* Adapted from: https://github.com/LongDirtyAnimAlf/ld64-msys
+* Author: LongDirtyAnimAlf
+*/
+#ifndef REALPATH_DEFINED
+#define REALPATH_DEFINED
+static inline char *realpath( const char *__restrict__ name, char *__restrict__ resolved ) {
+    char *retname = NULL;
+
+    if( name == NULL ) {
+        errno = EINVAL;
+        return NULL;
+    }
+    if (resolved == NULL) {
+        retname = (char*)malloc(_MAX_PATH);
+
+        if (retname == NULL) {
+            errno = ENOMEM;
+            return NULL;
+        }
+    } else {
+        retname = resolved;
+    }
+
+    if (_fullpath(retname, name, _MAX_PATH) == NULL) {
+        if (resolved == NULL) {
+            free(retname);
+        }
+        errno = ENAMETOOLONG;
+        return NULL;
+    }
+
+    char *p;
+    while ((p = strchr (retname, '\\')) != NULL) {
+        *p = '/';
+    }  
+
+    return retname;
+}
+#endif /* REALPATH_DEFINED */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
