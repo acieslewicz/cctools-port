aac1f501c5fe745702e37d0deefe9da61a587c5e
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-06-16 12:46:31 -0600

    Replace unix types with standard ctypes

--- cctools/libstuff/crc32.c
+++ cctools/libstuff/crc32.c
@@ -58,7 +58,7 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-static const u_int32_t crctab[] = {
+static const uint32_t crctab[] = {
 	0x0,
 	0x04c11db7, 0x09823b6e, 0x0d4326d9, 0x130476dc, 0x17c56b6b,
 	0x1a864db2, 0x1e475005, 0x2608edb8, 0x22c9f00f, 0x2f8ad6d6,
@@ -122,9 +122,9 @@ const void *buf,
 uint32_t len)
 {
 
-    register const u_char *p;
+    register const unsigned char *p;
     register int nr;
-    register u_int32_t crc;
+    register uint32_t crc;
 
     crc = 0;
     for (nr = len, p = buf; nr--; ++p) {
